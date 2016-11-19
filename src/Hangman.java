d60666b4c30b00167b7779482d71058ff585ca73
Ryguy-1 <league@WTS8.attlocal.net>
2016-11-18 18:28:36 -0800

    Almost Complete hangman

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,12 +1,18 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Hangman {
-	static String word = "apple";
-	static int lives = 5;
+	static String word = "";
+	static int lives = 6;
 	static Scanner s = new Scanner(System.in);
 	static Character c;
 	static ArrayList<Character> wordy = new ArrayList<Character>();
+	static String wordPrint = "";
+	static String guessChecker = "";
 
 	public static void main(String[] args) {
 		ArrayList<Character> wordChars = new ArrayList<Character>();
@@ -17,25 +23,89 @@ public class Hangman {
 		// Start
 		System.out.println("Hangman time!");
 		System.out.println("The word has " + word.length() + " letters.");
+		for (int i = 0; i < word.length(); i++) {
+			wordy.add('_');
+		}
+		for (Character c : wordy) {
+			wordPrint = wordPrint + "_";
+			guessChecker = wordPrint;
+		}
+
+		runner();
+	}
+
+	public static void start() {
 		System.out.println("You have " + lives + " lives.");
 		System.out.println("Time to guess!");
 		drawGallows();
 		System.out.println("Word:");
-		for (int i = 0; i < word.length(); i++) {
-			wordy.add("_");
-		}
-		System.out.println(wordy);
-		runner();
 	}
 
 	public static void runner() {
+		start();
+		System.out.println(wordPrint);
 		while (lives > 0) {
 			c = getGuess();
 			for (int i = 0; i < word.length(); i++) {
 				if (c == word.charAt(i)) {
-					// wordy = c;
+					for (int j = 0; j < word.length(); j++) {
+						if (word.charAt(j) == c) {
+							char[] changer = wordPrint.toCharArray();
+							changer[j] = c;
+							wordPrint = new String(changer);
+						}
+					}
 				}
 			}
+			if (wordPrint == guessChecker) {
+				lives--;
+			}
+			if (wordPrint == word) {
+				System.out.println("You win!!!!");
+				boolean l = false;
+				while (!l) {
+					System.out.println("Play again? (Yes or No)");
+					String guess = s.next();
+					if (guess.equals("No")) {
+						System.out.println("game over;");
+						l = true;
+						break;
+					}
+					if (guess.equals("Yes")) {
+						System.out.println("AGAIN! :D");
+						l = true;
+						lives = 6;
+						runner();
+					} else {
+						System.out.println("what?");
+					}
+				}
+			}
+			if (lives == 0) {
+				drawGallows();
+				System.out.println("You lose. The word was " + word + ".");
+				boolean l = false;
+				while (!l) {
+					System.out.println("Play again? (Yes or No)");
+					String guess2 = s.next();
+					if (guess2.equals("No")) {
+						System.out.println("game over;");
+						l = true;
+						break;
+					}
+					if (guess2.equals("Yes")) {
+						System.out.println("AGAIN! :D");
+						l = true;
+						lives = 6;
+						runner();
+					} else {
+						System.out.println("what?");
+					}
+				}
+			}
+			start();
+			System.out.println(wordPrint);
+			guessChecker = wordPrint;
 		}
 	}
 
@@ -43,7 +113,7 @@ public class Hangman {
 		String guess = s.next();
 		boolean l = false;
 		while (!l) {
-			if (guess.length() == 1) {
+			if (guess.length() != 1) {
 				System.out.println("Only one letter per guess.... you idiot");
 				guess = s.next();
 			} else {
@@ -55,8 +125,52 @@ public class Hangman {
 	}
 
 	public static void drawGallows() {
-		System.out.println(
-				"_______\n" + "|     |\n" + "|     \n" + "|    \n" + "|    \n" + "|_________\n" + "|________|\n");
+		if (lives == 6) {
+			System.out.println(
+					"_______\n" + "|     |\n" + "|     \n" + "|    \n" + "|    \n" + "|_________\n" + "|________|\n");
+		} else if (lives == 5) {
+			System.out.println(
+					"_______\n" + "|     |\n" + "|     O\n" + "|     \n" + "|    \n" + "|_________\n" + "|________|\n");
+		} else if (lives == 4) {
+			System.out.println("_______\n" + "|     |\n" + "|     O\n" + "|     |\n" + "|    \n" + "|_________\n"
+					+ "|________|\n");
+		} else if (lives == 3) {
+			System.out.println("_______\n" + "|     |\n" + "|     O\n" + "|     |\\ \n" + "|    \n" + "|_________\n"
+					+ "|________|\n");
+		} else if (lives == 2) {
+			System.out.println("_______\n" + "|     |\n" + "|     O\n" + "|    /|\\ \n" + "|    \n" + "|_________\n"
+					+ "|________|\n");
+		} else if (lives == 1) {
+			System.out.println("_______\n" + "|     |\n" + "|     O\n" + "|    /|\\ \n" + "|    /\n" + "|_________\n"
+					+ "|________|\n");
+		} else if (lives == 0) {
+			System.out.println("_______\n" + "|     |\n" + "|     O\n" + "|    /|\\ \n" + "|    / \\ \n"
+					+ "|_________\n" + "|________|\n");
+		}
+
+	}
+
+	public static String wordgen() {
+		int num = (int) Math.ceil(Math.random() * 1001);
+		try {
+			FileReader fr = new FileReader("HangmanWords.txt");
+			BufferedReader textReader = new BufferedReader(fr);
+			int numLines = 1001;
+			String[] textData = new String[numLines];
+			for (int i = 0; i < num; i++) {
+				textData[i] = textReader.readLine();
+			}
+			textReader.close();
+			return textData;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String word = "apple";
+		return word;
 	}
 }
 /*
---
+++ src/Warmup6Test.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+public class Warmup6Test {
+public static void main(String[] args) {
+}
+	@Test
+public static String[] initHighscores(){
+	public static void printHighscores(String[]HighScores)
+	public static Strig[] insertScore(String[] highscores, String newScore)
+}
+}
